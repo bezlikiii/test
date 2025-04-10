d25a4f2e610bca70627ac264e565f844720223b8
bezlikiii <dzusdaniil32@gmail.com>
2025-04-10 17:05:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('logo').onclick = function () {
+    const box = document.getElementById('list');
+    box.style.display = (box.style.display === 'none') ? 'block' : 'none';
+};
